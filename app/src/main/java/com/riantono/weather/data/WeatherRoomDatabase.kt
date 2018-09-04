f2f46cb9f08a838f6e5dea8efc95bf9b684c8bf6
Bernadus Billy Riantono <billyriantono@MBP.local>
2018-09-04 14:39:33 +0700

    Add : Weather DAO in Room

--- app/src/main/java/com/riantono/weather/data/WeatherRoomDatabase.kt
+++ app/src/main/java/com/riantono/weather/data/WeatherRoomDatabase.kt
@@ -4,15 +4,19 @@ import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import com.riantono.weather.BuildConfig
 import com.riantono.weather.data.dao.LocationDao
+import com.riantono.weather.data.dao.WeatherDao
 import com.riantono.weather.data.entity.Location
+import com.riantono.weather.data.entity.Weather
 
 @Database(
         entities = [
-            Location::class
+            Location::class,
+            Weather::class
         ],
         version = BuildConfig.ROOM_DATABASE_VERSION,
         exportSchema = false
 )
 abstract class WeatherRoomDatabase : RoomDatabase() {
     abstract fun locationDao(): LocationDao
+    abstract fun weatherDao(): WeatherDao
 }

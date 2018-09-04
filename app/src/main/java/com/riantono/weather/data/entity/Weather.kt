8b7d397d40c24766cb216d36af50de8a939babbe
Bernadus Billy Riantono <billyriantono@MBP.local>
2018-09-04 14:17:21 +0700

    Init : Configure Properties

--- app/src/main/java/com/riantono/weather/data/entity/Weather.kt
+++ app/src/main/java/com/riantono/weather/data/entity/Weather.kt
@@ -1,17 +1,20 @@
 package com.riantono.weather.data.entity
 
 import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Embedded
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.ForeignKey.CASCADE
 import android.arch.persistence.room.PrimaryKey
 
-
-@Entity(tableName = "tbl_location")
-class Location {
+@Entity(tableName = "tbl_weather",
+        foreignKeys = [(ForeignKey(entity = Location::class,
+                parentColumns = arrayOf("id"),
+                childColumns = arrayOf("location_id"), onDelete = CASCADE))])
+class Weather {
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0
-    @ColumnInfo(name = "latitude")
-    val latitude: Double = 0.0
+    @ColumnInfo(name = "location_id")
+    val locationId: Int = 0
     @ColumnInfo(name = "longitude")
     val longitude: Double = 0.0
     @ColumnInfo(name = "address")

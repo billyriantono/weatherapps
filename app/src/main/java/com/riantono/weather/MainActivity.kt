0baa325954b6aa1578eb4a7e7e1d65c7235d50c9
Bernadus Billy Riantono <billyriantono@android-1c6168aa0bd3cdb0.icehousecorp.local>
2018-09-06 13:48:00 +0700

    Init : Using Navigation

--- app/src/main/java/com/riantono/weather/MainActivity.kt
+++ app/src/main/java/com/riantono/weather/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.riantono.weather
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +20,23 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(mToolbar)
 
         supportActionBar?.title = "Today in Jakarta"
+
+        var mButtonAddCity: Button = findViewById(R.id.btn_add_search_city)
+        mButtonAddCity.setOnClickListener { Navigation.createNavigateOnClickListener(R.id.goToSettingAction) }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.top_menu_app, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_settings -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

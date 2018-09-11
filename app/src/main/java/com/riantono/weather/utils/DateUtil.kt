af1f299f09035e2a471500d14bbff800195a4552
Bernadus Billy Riantono <bernadus.riantono@icehousecorp.com>
2018-09-11 20:06:10 +0700

    Change: Date Format

--- app/src/main/java/com/riantono/weather/utils/DateUtil.kt
+++ app/src/main/java/com/riantono/weather/utils/DateUtil.kt
@@ -6,9 +6,9 @@ import java.util.*
 class DateUtil {
     companion object {
         val FORMAT_DATE_Y_M_D = "yyyy-MM-dd"
-        val FORMAT_DATE_Y_M_D_H_M_S = "yyyy-MM-dd hh:mm:ss"
+        val FORMAT_DATE_D_M_Y_H_M_S = "dd MMMM yyyy HH:mm:ss"
         fun formatDate(timestamp: Long): String {
-            val simpleDateFormat = SimpleDateFormat(FORMAT_DATE_Y_M_D_H_M_S, Locale.US)
+            val simpleDateFormat = SimpleDateFormat(FORMAT_DATE_D_M_Y_H_M_S, Locale.US)
             val date = Date(timestamp * 1000) //  times by 1000 because it's epoch timestamp
             return simpleDateFormat.format(date)
         }

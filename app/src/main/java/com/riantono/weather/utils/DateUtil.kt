dd2d52f885388cec03e0de90b9896c8f52a7c84f
Bernadus Billy Riantono <bernadus.riantono@icehousecorp.com>
2018-09-11 20:04:11 +0700

    Fixing : UI Forecast

--- app/src/main/java/com/riantono/weather/utils/DateUtil.kt
+++ app/src/main/java/com/riantono/weather/utils/DateUtil.kt
@@ -6,8 +6,9 @@ import java.util.*
 class DateUtil {
     companion object {
         val FORMAT_DATE_Y_M_D = "yyyy-MM-dd"
+        val FORMAT_DATE_Y_M_D_H_M_S = "yyyy-MM-dd hh:mm:ss"
         fun formatDate(timestamp: Long): String {
-            val simpleDateFormat = SimpleDateFormat(FORMAT_DATE_Y_M_D, Locale.US)
+            val simpleDateFormat = SimpleDateFormat(FORMAT_DATE_Y_M_D_H_M_S, Locale.US)
             val date = Date(timestamp * 1000) //  times by 1000 because it's epoch timestamp
             return simpleDateFormat.format(date)
         }

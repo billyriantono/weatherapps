b993dbd8071020b4f71402533194085c4a8664ef
Bernadus Billy Riantono <bernadus.riantono@icehousecorp.com>
2018-09-13 11:11:34 +0700

    Remove : Date Util

--- app/src/main/java/com/riantono/weather/ui/fragments/citydetail/adapters/viewholders/ForecastListViewHolder.kt
+++ app/src/main/java/com/riantono/weather/ui/fragments/citydetail/adapters/viewholders/ForecastListViewHolder.kt
@@ -7,6 +7,16 @@ import android.widget.TextView
 import com.riantono.weather.R
 import com.riantono.weather.ui.entity.ForecastModel
 import com.riantono.weather.utils.DateUtil
+import java.text.SimpleDateFormat
+import java.util.*
+
+val FORMAT_DATE_D_M_Y_H_M_S = "dd MMMM yyyy HH:mm:ss"
+
+fun formatDate(timestamp: Long): String {
+    val simpleDateFormat = SimpleDateFormat(FORMAT_DATE_D_M_Y_H_M_S, Locale.US)
+    val date = Date(timestamp * 1000) //  times by 1000 because it's epoch timestamp
+    return simpleDateFormat.format(date)
+}
 
 class ForecastListViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
     val tvWeatherDegree = itemView?.findViewById<TextView>(R.id.tv_weather_degree)
@@ -16,7 +26,7 @@ class ForecastListViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView
     fun bindData(context: Context, forecastModel: ForecastModel) {
         tvWeatherDegree?.text = context.resources.getString(R.string.degree_placeholder, forecastModel.temp.toString())
         if (forecastModel.dt != null) {
-            tvWeatherDate?.text = DateUtil.formatDate(forecastModel.dt)
+            tvWeatherDate?.text = formatDate(forecastModel.dt)
         } else {
             tvWeatherDate?.text = "Unknown Date"
         }

b481ce0e8ac93f75a640841a8b11a942fe4ee78f
Bernadus Billy Riantono <bernadus.riantono@icehousecorp.com>
2018-09-13 11:11:46 +0700

    Remove : Date Util

--- app/src/main/java/com/riantono/weather/ui/fragments/citydetail/adapters/viewholders/ForecastListViewHolder.kt
+++ app/src/main/java/com/riantono/weather/ui/fragments/citydetail/adapters/viewholders/ForecastListViewHolder.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.widget.TextView
 import com.riantono.weather.R
 import com.riantono.weather.ui.entity.ForecastModel
-import com.riantono.weather.utils.DateUtil
 import java.text.SimpleDateFormat
 import java.util.*
 
